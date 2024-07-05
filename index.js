6733eae3841529900c1d80c58432023abd557e70
Dasha Konchyts <forexamplednk@gmail.com>
2024-07-06 00:08:02 +0300

    add: fourth task solution

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 /*Создайте переменные a1, a2, a3, a4, a5, a6, a7 (нужна для решения 4 задания), a8, a9,
 a10, a11, a12, a13, a14, a15, a16, a17, a18. Поместите в них и выведите в консоль
 результат выражений:
@@ -41,3 +42,13 @@ let a17 = false || true;
 let a18 = true > 0;
 console.log(typeof a1, typeof a2, typeof a3, typeof a4, typeof a5, typeof a6, typeof a7, typeof a8, typeof a9, typeof a10,
   typeof a11, typeof a12, typeof a13, typeof a14, typeof a15, typeof a16, typeof a17, typeof a18);
+
+/*Напиши скрипт, который находит объем цилиндра высотой 10м и диаметром
+основания равным значению переменной a7 из задания 2, результат поместите в
+переменную VCilindra.  */
+
+// V=πr2h
+let HCilindra = 10;
+let DCilindra = a7;
+let VCilindra = Math.PI * Math.pow((DCilindra / 2), 2) * HCilindra;
+console.log(VCilindra);
