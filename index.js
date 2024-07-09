7f166c17238ffa68210b996c41713945ef603c80
Dasha Konchyts <forexamplednk@gmail.com>
2024-07-09 23:44:15 +0300

    add: second task solution

--- index.js
+++ index.js
@@ -1,73 +1,11 @@
-
-/*Создайте переменные a1, a2, a3, a4, a5, a6, a7 (нужна для решения 4 задания), a8, a9,
-a10, a11, a12, a13, a14, a15, a16, a17, a18. Поместите в них и выведите в консоль
-результат выражений:
-5 % 3;
-3 % 5;
-5 + '3';
-'5' – 3;
-75 + 'кг';
-785 * 653;
-100 / 25;
-0 * 0;
-0 / 2;
-89 / 0;
-98 + 2;
-5 – 98;
-(8 + 56 * 4) / 5;
-(9 - 12) * 7 / (5 + 2);
-+"123";
-1 || 0;
-false || true;
-true > 0.
-Проверьте (выведите в консоль) каким типам принадлежат переменные. 
+/*2 Выведите столбец чисел от 89 до 11 - воспользуйтесь циклом while и отделите числа
+тегом <br /> друг от друга, чтобы получить столбец, а не строку.
  */
-let a1 = 5 % 3;
-let a2 = 3 % 5;
-let a3 = 5 + '3';
-let a4 = 5 - '3';
-let a5 = 75 + 'кг';
-let a6 = 785 * 653;
-let a7 = 100 / 25;
-let a8 = 0 * 0;
-let a9 = 0 / 2;
-let a10 = 89 / 0;
-let a11 = 98 + 2;
-let a12 = 5 - 98;
-let a13 = (8 + 56 * 4) / 5;
-let a14 = (9 - 12) * 7 / (5 + 2);
-let a15 = +"123";
-let a16 = 1 || 0;
-let a17 = false || true;
-let a18 = true > 0;
-console.log(typeof a1, typeof a2, typeof a3, typeof a4, typeof a5, typeof a6, typeof a7, typeof a8, typeof a9, typeof a10,
-  typeof a11, typeof a12, typeof a13, typeof a14, typeof a15, typeof a16, typeof a17, typeof a18);
-
-/*Напиши скрипт, который находит объем цилиндра высотой 10м и диаметром
-основания равным значению переменной a7 из задания 2, результат поместите в
-переменную VCilindra.  */
-
-// V=πr2h
-let HCilindra = 10;
-let DCilindra = a7;
-let VCilindra = Math.PI * Math.pow((DCilindra / 2), 2) * HCilindra;
-console.log(VCilindra);
-
-/*Найдите площадь круга (SKruga) с радиусом 5см (r). 
- */
-let r = 5;
-let SKruga = Math.PI * Math.pow(r, 2);
-console.log(SKruga);
-
-/* Найдите площадь трапеции (STrap) с основаниями 5см (a) и 7см (b), и высотой 10см
-(h).  */
-//S = (a + b) * h / 2
 {
-  let STrap;
-  let a = 5;
-  let b = 7;
-  let h = 10;
-  STrap = (a + b) * h / 2;
-  console.log(STrap);
+  let i = 89;
+while (i >= 11) {
+  document.write(i + `<br>`);
+  i--;
 }
 
+}
