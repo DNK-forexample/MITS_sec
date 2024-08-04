5e42f4d335a0a70ce27fa3577e714707fbcd7cc4
Dasha Konchyts <forexamplednk@gmail.com>
2024-08-05 00:32:58 +0300

    add: fifth task solution

--- index.js
+++ index.js
@@ -61,3 +61,16 @@ let b = 10
 let arr = returnArray(a, b);
 printArray(arr);
 }
+
+/*5. Сделайте функцию isEven() (even - это четный), которая параметром
+принимает целое число и проверяет: четное оно или нет. Если четное - пусть
+функция возвращает true, если нечетное — false. */
+
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isEven(10));
