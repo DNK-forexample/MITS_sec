0edba9a5ab3e0c77ca2d1d695cd2009ef8965df1
Dasha Konchyts <forexamplednk@gmail.com>
2024-08-04 23:40:33 +0300

    add: second task solution

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function getResult(a, b, c) {
   return (a - b) / c;
 }
 console.log(getResult(10, 4, 2));
+
+/*2. Сделайте функцию, которая возвращает куб числа и его квадрат. Число
+передается параметром. */
+function getCubeOfNumber(number) {
+  return Math.pow(number, 3);
+}
+console.log(getCubeOfNumber(3));
+function getSquareOfNumber(number) {
+  return Math.pow(number, 2);
+}
+console.log(getSquareOfNumber(3));
+
+
